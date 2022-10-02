7b975620e7356e8fe0e69439092688410d589917
Camilo Tabares <camilo@MacBook-Pro-de-Camilo.local>
2022-10-02 10:20:04 -0500

    First commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ButtonBase } from "@mui/material";
+
+const Button = ({...props}) => {
+  return (
+    <ButtonBase {...props} />
+  )
+}
+
+export default Button;
+
